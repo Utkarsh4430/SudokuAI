e969d7cfa748e3ccacfe8e6fe80e08302fbce0ee
utkarsh4430 <utkarsh4430@gmail.com>
2020-01-10 02:27:34 +0530

    removed boundaries and centered image

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ import matplotlib.pyplot as plt
 import os
 from preprocessing import preprocess
 from matrix_gen import matrix_generator
-
-image_path = './image.jpg'
+# from solver import solve
+image_path = './testing.png'
 SQUARE_IMG_DIM = (81,81)
 img = cv2.imread(image_path,cv2.IMREAD_GRAYSCALE)
 img = preprocess(img,SQUARE_IMG_DIM)
 img = cv2.resize(img,(900,900),interpolation=cv2.INTER_AREA)
-# plt.imshow(img,cmap='gray')
-# plt.show()
+plt.imshow(img,cmap='gray')
+plt.show()
 matrix = matrix_generator(img)
+print("Given Sudoku:")
 for i in range(9):
     for j in range(9):
         if(matrix[i][j]==-1):
@@ -21,8 +22,18 @@ for i in range(9):
         print(matrix[i][j],end = ' ')
     print()
 
-# try with gaussian filter next
-# correct model
+# print('Solved Sudoku:')
+
+# solution = solve(matrix)
+# for i in range(9):
+#     for j in range(9):
+#         print(solution[i][j],end = ' ')
+#     print()
+
+
+
+# try with gaussian filter next - didnt work
+# correct model 
 # code solver
 # organise
 # readme
--- matrix_gen.py
+++ matrix_gen.py
@@ -3,10 +3,75 @@ import numpy as np
 from matplotlib import pyplot as plt
 from keras.models import load_model
 model = load_model("./test_model")
+# visited = np.array()
+
+def centering_number(box,visited):
+    up = down = left = right = -1
+    for i in range(visited.shape[0]):
+        for j in visited[i]:
+            if(j==1):
+                up = i
+                break
+        if not up==-1:
+            break
+    for i in range(visited.shape[0]-1,-1,-1):
+        for j in visited[i]:
+            if j==1:
+                down = i
+                break
+        if not down==-1:
+            break
+    for i in range(visited.shape[1]):
+        for j in visited[:,i]:
+            if(j==1):
+                left = i
+                break
+        if not left==-1:
+            break
+    for i in range(visited.shape[1]-1,-1,-1):
+        for j in visited[:,i]:
+            if j==1:
+                right = i
+                break
+        if not right==-1:
+            break
+    width = right-left
+    height = down-up
+    new_box = np.zeros(box.shape)
+    new_box[(box.shape[0]-height)//2:(box.shape[0]-height)//2 + height, (box.shape[1]-width)//2:(box.shape[1]-width)//2 + width] = box[up:down,left:right]
+    return new_box        
+
+def connected_comp(box,visited,i,j):
+    if box[i][j]==0 or visited[i][j]==1:
+        return visited
+    visited[i][j] = 1
+    visited = connected_comp(box,visited,i,j+1)
+    visited = connected_comp(box,visited,i,j-1)
+    visited = connected_comp(box,visited,i-1,j)
+    visited = connected_comp(box,visited,i+1,j)
+    return visited
+
+def removing_boundary(box):
+    visited = np.zeros(box.shape)
+    for i in range(30,70):
+        for j in range(30,70):
+            if(visited[i][j]):
+                continue
+            if box[i][j]==0:
+                continue
+            else:
+                visited = connected_comp(box,visited,i,j)
+    for i in range(box.shape[0]):
+        for j in range(box.shape[1]):
+            if not visited[i][j]:
+                box[i][j] = 0
+    return box,visited
+    
 
 def predict(img):
     img = cv2.resize(img,(28,28),interpolation = cv2.INTER_AREA)
     # img = 255.0 - img
+    img = cv2.GaussianBlur(img.copy(), (1,1), 0)
     # plt.imshow(img,cmap='gray')
     # plt.show()
     # print(type(img[0][0]))
@@ -40,7 +105,13 @@ def matrix_generator(img):
             if(calc(x)<10):
                 matrix[i,j] = -1
             else:
-                matrix[i,j] = predict(box[10:90,10:90])
+                # plt.imshow(box)
+                # plt.show()
+                improved_box,visited = removing_boundary(box)
+                centered_box = centering_number(improved_box,visited)
+                matrix[i,j] = predict(centered_box[10:90,10:90])
+                # plt.imshow(centered_box)
+                # plt.show()
             # plt.imshow(x, cmap = 'gray')
             # plt.show()
             # matrix[i,j] = predict(box)
